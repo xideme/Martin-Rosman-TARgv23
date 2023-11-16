a6b0ab92a52d578c2d524cfdbc44aab7efe6950e
opilane <opilane@HP-CZC2349HTS>
2023-11-16 11:48:24 +0200

    Add project files.

---
+++ MR-TARgv23-First/MR_TARgv23_First.py
@@ -0,0 +1,6 @@
+print("Tere tulemast!")
+kool=input("Mis koolis sa õpid?: ") #str kool
+kursus=int(input("Mis kursusel?: ")) #int kursus
+print("Tere tulemast kooli"+kool+"\n Ole hea "str(kursus)+".kursuse õpilaseks!")
+print("Tere tulemast kooli", kool, "!\Ole hea", kursus, ".kursuse õpilaseks!")
+print("Tere tulemast kooli {0}!\nOle hea {1}.kursuse õpetajaks!".format(kool,kursus))
